5d57f9a48206e89a076524a33dc1a4dcae2cf51b
Luiz Henrique <132685394+Luizdexx@users.noreply.github.com>
2024-07-24 12:29:32 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+document.getElementById('increase-font').addEventListener('click', function() {
+    document.body.style.fontSize = 'larger';
+});
+
+document.getElementById('decrease-font').addEventListener('click', function() {
+    document.body.style.fontSize = 'smaller';
+});
+
+
